2d771581e72a14bdcc73a1ddbb80357e0fc31556
João Moreira <joaomoreira.a.s@gmail.com>
2023-12-15 18:21:10 -0300

    feat: create home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 15 de Dezembro de 2023</Text>
+    </View>
+  );
+}
