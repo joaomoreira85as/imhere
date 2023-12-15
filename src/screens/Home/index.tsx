09c8192d43078e05e12ca4a61aed48c610db908c
João Moreira <joaomoreira.a.s@gmail.com>
2023-12-15 19:20:15 -0300

    create add participant input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
+  const handleParticipantAdd = () => {
+    console.log("Clicou para adicionar um participante");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 15 de Dezembro de 2023</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,4 +15,32 @@ export const styles = StyleSheet.create({
     color: "#6B6B6B",
     fontSize: 16,
   },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: "#1F1E25",
+    borderRadius: 5,
+    color: "#FDFCFE",
+    padding: 16,
+    fontSize: 16,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 28,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+  },
+  form: {
+    width: "100%",
+    marginTop: 26,
+    marginBottom: 36,
+    flexDirection: "row",
+    columnGap: 7,
+  },
 });
